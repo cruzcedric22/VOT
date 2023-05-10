6e7de2f404f8d4ad4fb9f26e78797a81397f92d2
cruzcedric22 <cruzcedric66@gmail.com>
2023-05-10 12:05:41 +0800

    UI update

--- index.php
+++ index.php
@@ -47,15 +47,20 @@ if($user_log != "" && $user_cat == 'Admin'){
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
   <!-- <link rel="stylesheet" href="css/bootstrap.min.css" /> -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
-  <link rel="stylesheet" href="css/style.css" />
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script> -->
+  <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" /> -->
+  <!-- <link rel="stylesheet" href="css/style.css" /> -->
   <link rel="icon" href="favicon.ico">
+   <!-- Bootstrap CSS -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/login.css">
+
   <style>
     a{
       text-decoration: none;
     }
+   
   </style>
 </head>
 <div>
@@ -65,53 +70,49 @@ if($user_log != "" && $user_cat == 'Admin'){
 ?>
 </div>
 <body>
-       
-        
-        <div class="container">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="circle"></div>
-                        <header class="myHed text-center">
-                          <i class="far" style="height: 118px;"></i>
-                          <p>Login</p>
-                        </header>
-                        <form id="login_form" class="main-form text-center" method="post">
-                            <div class="form-group my-0">
-                                <label class="my-0">
-                                    <i class="fas fa-user"></i>
-                                    <input type="text" class="myInput username" id="username" name="username" placeholder="USERNAME"> <!-- FOR USERNAME -->
-                                  </label>
+
+<center>
+        <div class="container login">
+            <div class="card container login-container bg-light shadow">
+                <div class="col-sm-12">
+                    <div class="row">
+                        <!-- column left -->
+                        <div class="col-sm-6 column-left">
+                            <img src="img/img_ucc.png" alt="">
+                            <div>
+                                <label for="" class="ucc-name">UNIVERSITY OF CALOOCAN CITY</label> <br>
+                                <label for="" class="address">Biglang Awa St Cor 11th Ave Catleya, East Grace Park, Caloocan, 1400 Metro Manila</label>
                             </div>
-                            <div class="form-group my-0">
-                                <label class="my-0">
-                                    <i class="fas fa-lock"></i>
-                                    <input type="password" class="myInput password" id="pass" name="pass" placeholder="PASSWORD"> <!-- FOR PASSWORD -->
-                                  </label>
+                        </div>
+                        <!-- column right -->
+                        <div class="col-sm-6 column-right">
+                        <form id="login_form" method="post">
+                            <label for="" class="login-text">Login</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control form-control-lg" id="username" name="username" placeholder="Username">
                             </div>
-                            
-                            <!-- <label class="check_1">
-                                <input type="checkbox" checked>
-                                Remember Me
-                            </label> -->
-                            <div class="form-group">
-                                <label> 
-                                    <input type="submit" class="form-control button" id="login" name="login" value="LOGIN"> <!-- FOR LOGIN BTN -->
-                                  </label>
+                            <div class="input-group">
+                                <input type="password" class="form-control form-control-lg" id="pass" name="pass" placeholder="Password">
                             </div>
-                            <div class="form-group">
-                                <label>  
-                                    <a href="regis.php"><input type="button" class="form-control button regis mt-3"  name="create" value="REGISTRATION"></a>
-                               </label> 
+                            <div class="input-group">
+                                <button type="button" class="btn btn-lg w-100 shadow-sm" id="login" name="login" value="LOGIN">Login</button>
                             </div>
-                             <a href="forget_pass.php" class="check_1">Forget Password</a> 
-                         </form>
+                          </form>
+                            <a href="forget_pass.php" class="forgot-password">Forgot Password?</a> <br>
+                            <label for="" class="account">Don't have an account yet? <a href="regis.php" class="register-link">Register here.</a></label>
+                        </div>
                     </div>
                 </div>
             </div>
-          </div>
+        </div>
+    </center>
+       
+        
+      
     
 
-
+ <!-- Bootstrap JS -->
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
   <script src = "js/jquery-3.6.1.min.js"></script>
   <script src="js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   <script src="js/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
--- regis.php
+++ regis.php
@@ -15,13 +15,20 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="css/style1.css" />
+  <!-- <link rel="stylesheet" href="css/style1.css" /> -->
+     <!-- Bootstrap CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- login CSS -->
+    <link rel="stylesheet" href="css/regis.css">
   <style>
     .validationText{
       font-size: small;
       text-align: left;
       padding: 0;
       margin: 0;
+    }
+    .register-text{
+      color: #ff7b00;
     }
   </style>
 </head>
@@ -49,82 +56,106 @@ $result2 = mysqli_query($conn,$dropcourse);
 //         }
 // }
 
-
-
-
-
-
 ?>
 
-<div class="container">
-                <div class="card regis" style="height: fit-content;">
-                    <div class="card-body">
-                        <div class="circle"></div>
-                        <header class="myHed text-center">
-                          <p>Registration</p>
-                        </header>
-                        <form id="registerForm" class="main-form text-center" method="post">
+<center>
+        <div class="container register">
+        <form id="registerForm" class="main-form text-center" method="post">
+            <div class="card container register-container bg-light shadow">
+                <label for="" class="register-text">Registration</label>
+
+                <!-- student no. and course -->
+                <div class="col-sm-12">
+                    <div class="row">
+                        <div class="col-sm-6">
                             <div class="form-group">
-                               <b><label class="form-label" style="color: #202020;">Student Number: <span style="color: red;">*</span></label></b>
-                                    <input type="text" class="form-control" id="stdno" name="stdno" placeholder="STUDENT NO:" required onkeyup="validationRegis()" onchange="validateRegisForm()" > <!-- FOR STUDENT NO/ -->
-                                    <p class="validationText" id="txtStdnoVal"></p>
+                                <input type="text" class="form-control form-control-lg" id="stdno" name="stdno" placeholder="Student No." onkeyup="validationRegis()">
+                                <p class="validationText" id="txtStdnoVal"></p>
                             </div>
+                        </div>
+                        <div class="col-sm-6">
                             <div class="form-group">
-                                <b><label class="form-label" style="color: #202020;">Given Name: <span style="color: red;">*</span></label></b>
-                                    <input type="text" class="form-control" id="fname" name="fname" placeholder="NAME" required> <!-- FOR NAME -->
-                            </div>
-                            <div class="form-group">
-                                <b><label class="form-label" style="color: #202020;">Middle Initial: <span style="color: red;">*</span></label></b>
-                                    <input type="text" class="form-control" id="miniital" name="m_initial" placeholder="MIDDLE INITIAL" required> <!-- FOR MIDDLE INITIAL -->
-                            </div>
-                            <div class="form-group">
-                               <b><label class="form-label" style="color: #202020;">Last Name: <span style="color: red;">*</span></label></b>
-                                    <input type="text" class="form-control" id="lname" name="lname" placeholder="LAST NAME" required> <!-- FOR MIDDLE INITIAL -->
-                            </div>
-                            <div class="form-group">
-                                <b><label class="form-label" style="color: #202020;">Course: <span style="color: red;">*</span></label></b>
-                                <select class="form-select" name="course" aria-label="Default select example">
-                                    
-                                       <?php foreach($result2 as $row1){
+                                <select name="" id="" class="form-control form-control-lg" name="course">
+                                <?php foreach($result2 as $row1){
                                             ?>
                                                 <option value="<?php echo $row1['course_id'];?>"><?php echo $row1['course_name'];?></option>
                                             <?php
                                         } ?>
-                                      
-                                        </select> <!-- FOR COURSE -->
+                                </select>
                             </div>
-                            <div class="form-group">
-                               <b><label class="form-label" style="color: #202020;">Email: <span style="color: red;">*</span></label></b>
-                                    <input type="email" class="form-control" id="email" name="email" placeholder="EMAIL" required onkeyup="validationEmailRegis()" onchange="validateRegisForm()"> <!-- FOR EMAIL -->
-                                    <p class="validationText" id="txtEmailVal"></p>
-                            </div>
-                            <div class="form-group" style="color: #202020;">
-                                <b><label class="form-label">Username:<span style="color: red;">*</span></label></b>
-                                    <input type="text" class="form-control" id="username" name="username" placeholder="USERNAME" required onkeyup="validationUsernameRegis()"> <!-- FOR USERNAME -->
-                                    <p class="validationText" id="txtUsernameVal"></p>
-                            </div>
-                            <div class="form-group" style="color: #202020;">
-                               <b><label class="form-label">Password: <span style="color: red;">*</span></label></b>
-                                    <input type="password" class="form-control" id="pass" name="pass" placeholder="PASSWORD" required onkeyup="validateRegisForm()"> <!-- FOR PASSWORD -->
-                            </div>
-                            <div class="form-group" style="color: #202020;">
-                                <b><label class="form-label">Confirm password: <span style="color: red;">*</span></label></b>
-                                    <input type="password" class="form-control" id="pass1" name="con_pass" placeholder="CONFIRM PASSWORD" onkeyup="validateRegisForm()" required> <!-- FOR PASSWORD -->
-                                    <p class="validationText" id="txtPassVal"></p>
-                            </div>
-                            
-                            
-                            <div class="form-group">
-                                <label> 
-                                    <input type="submit" class="form-control button" id="regis" name="regis" value="REGISTER"> <!-- FOR LOGIN BTN -->
-                                  </label>
-                            </div>
-                            
-                          </form>
+                        </div>
                     </div>
                 </div>
+
+                <!-- name (first name, middle initial, last name) -->
+                <div class="col-sm-12">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-lg" id="fname" name="fname" placeholder="First Name">
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-lg" id="miniital" name="m_initial" placeholder="Middle Initial">
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-lg" id="lname" name="lname" placeholder="Last Name">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- email and username -->
+                <div class="col-sm-12">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-lg" id="email" name="email" placeholder="Email" onkeyup="validationEmailRegis()"> 
+                                <p class="validationText" id="txtEmailVal"></p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-lg" id="username" name="username" placeholder="Username" onkeyup="validationUsernameRegis()">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- password and confirm password -->
+                <div class="col-sm-12">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input type="password" class="form-control form-control-lg" name="pass" id="pass" placeholder="Password" onkeyup="validateRegisForm()">
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input type="password" class="form-control form-control-lg" id="pass1" name="con_pass" placeholder="Confirm Password" onkeyup="validateRegisForm()">
+                                <p class="validationText" id="txtPassVal"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+          </form>
+                
+                <!-- register button and login here link -->
+                <div class="form-group">
+                    <button type="button" class="btn btn-lg w-100 shadow-sm" id="regis" name="regis">Register</button>
+                </div>
+                <div class="form-group">
+                    <label for="" class="account">Have an account already? <a href="index.php" class="login-link">Login here.</a></label>
+                </div>
             </div>
-          </div>
+        </div>
+    </center>
+
+
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -407,7 +438,7 @@ $result2 = mysqli_query($conn,$dropcourse);
                 $('#pass').css('background-color', '#F7AA97');
                 $('#pass1').css('background-color', '#F7AA97');
                 $('#txtPassVal').html("Password didn'nt match");
-                  $('#txtPassVal').css('color', 'red');
+                $('#txtPassVal').css('color', 'red');
 
               }
 
